6ec4dc5829ddefdbf6e62e6d04f4ca5b96ec81cc
RayenBSD <92104474+RayenBSD@users.noreply.github.com>
2022-03-03 22:25:05 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+function BubbleSort (arr) {
+
+    let z;
+    
+    for (let i = 0; i < size - 1; i++) {
+        for (let j = 0; j < size - i - 1; j++) {
+            if (arr[j] < arr[j+1]) {
+                z = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = z;
+            }
+        }
+    }
+    return arr;
+}
+    
+    
+let size = prompt("enter the size: ");
+let arr = [];
+    
+for (let i = 0; i < size; i++) {
+    let x = prompt("enter a number: ");
+    arr[i] = x; 
+}
+    
+let arr1 = BubbleSort(arr);
+    
+for (let value of arr1){
+    console.log(value);
+}
